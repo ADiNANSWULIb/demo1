8104378b79c74bd145272115e32f993d0945d541
Adinan <adinan.swulib@gmail.com>
2022-07-25 20:15:49 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+echo "Hello World 1";
+?>
+// Git
+/*
+git init
+git add .
+
+git config --global user.email "adinan.swulib@gmail.com"
+git config --global user.name "Adinan"
+
+git commit -m "first"
+git remote add origin https://github.com/ADiNANSWULIb/TEST-YRU.git
+git push -u origin main
+
+git add . && git commit -m "first1" && git push
+
+git add .
+git commit -m "first1"
+git push
+*/
