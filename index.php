31e8ee8448d83620aa976f01818cb027a61c5fcd
Adinan <adinan.swulib@gmail.com>
2022-07-25 20:19:43 +0700

    1

--- index.php
+++ index.php
@@ -9,8 +9,9 @@ git add .
 git config --global user.email "adinan.swulib@gmail.com"
 git config --global user.name "Adinan"
 
-git commit -m "first"
-git remote add origin https://github.com/ADiNANSWULIb/TEST-YRU.git
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ADiNANSWULIb/demo1.git
 git push -u origin main
 
 git add . && git commit -m "first1" && git push
